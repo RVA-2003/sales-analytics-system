0758ad4e2b536f05e839847d1cbe68fb913a0b85
Rachit Acharya <rachit.v.acharya@gmail.com>
2026-01-19 20:46:50 +0530

    Remove unused template code from api_handler

--- utils/api_handler.py
+++
@@ -1,8 +0,0 @@
-from utils.file_handler import clean_sales_data
-from utils.data_processor import analyze_sales_data
-
-
-def get_sales_summary(file_path):
-    cleaned_data = clean_sales_data(file_path)
-    analysis_result = analyze_sales_data(cleaned_data)
-    return analysis_result
